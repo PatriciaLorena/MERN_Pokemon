3fa2f8220d0cfb720463e933d215ae8d9e62befd
Paty <patrilorenfer@gmail.com>
2024-03-16 18:16:39 -0300

    axiosPokemon

---
+++ AxiosPokemon/src/assets/components/AxiosPokemon.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AxiosPokemon = () => {
+    const [pokemonList, setPokemonList] = useState([]);
+    const [fetchPokemon, setFetchPokemon] = useState(false);
+
+    useEffect(() => {
+        if (fetchPokemon) {
+            axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+                .then(response => {
+                    setPokemonList(response.data.results.map(pokemon => pokemon.name));
+                })
+                .catch(error => {
+                    console.error('Error al recuperar Pokémon:', error);
+                });
+        }
+        setFetchPokemon(false);
+    }, [fetchPokemon]);
+
+    return (
+        <>
+            <div className='container d-flex justify-content-center mt-3'>
+                <button className='btn btn-dark text-white p-3 font-weight-bold fs-4' onClick={() => setFetchPokemon(true)}>Fetch Pokémon</button>
+            </div>
+            <div className="mt-container d-flex justify-content-center mt-3'">
+                {pokemonList.length > 0 && (
+                    <ul>
+                        {pokemonList.map((pokemon, index) => (
+                            <li key={index}>{pokemon}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default AxiosPokemon;
